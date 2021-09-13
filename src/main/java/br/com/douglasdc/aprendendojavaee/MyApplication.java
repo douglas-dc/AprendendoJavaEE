f196b2f78f61e101012a23ce7a625c358e9dcb42
Douglas Daniel Casagrande <douglasdcasagrande@hotmail.com>
2021-09-13 11:14:20 -0300

    First commit

---
+++ src/main/java/br/com/douglasdc/aprendendojavaee/MyApplication.java
@@ -0,0 +1,9 @@
+package br.com.douglasdc.aprendendojavaee;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class MyApplication extends Application {
+    
+}
